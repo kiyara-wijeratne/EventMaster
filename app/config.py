a8a7b35a2cff7da6b8dfeaa03a205400d2d40d97
kiyara <hwijerat@jaguarlandrover.com>
2026-05-27 15:45:29 +0100

    initial setup and config

---
+++ app/config.py
@@ -0,0 +1,9 @@
+class Config(object):
+    TESTING = False 
+    
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/eventmaster.db'
+    
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
